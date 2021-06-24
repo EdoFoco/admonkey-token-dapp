2051d27f27f2dc561b7c8775411fdd6b7469faac
Andrea Longhitano <andlon@MAC-UK-ANDLON.station>
2021-06-25 00:11:25 +0200

    Removed unneeded code

--- space-monkey-fe/src/components/Rewards/rewardsDashboard/ClaimReward.js
+++ space-monkey-fe/src/components/Rewards/rewardsDashboard/ClaimReward.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Title from './Title';
 import PropTypes from 'prop-types';
-import { withAppContextConsumer } from '../../../services/app-context';
-import { withDrizzleContextConsumer } from '../../../services/drizzle';
-import SpaceMonkeyContract from '../../../contracts/SpaceMonkey';
-import { getTokenTransactionsForWallet } from '../../../services/bsscan';
 import _ from 'lodash';
 
 export default function RewardsContainer({reward, onClaimReward, disabled}) {
