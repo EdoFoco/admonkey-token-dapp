9aeda78846abf5f530de1fb330061c02ee8efea7
Edoardo Foco <e.foco@hotmail.it>
2021-07-07 22:51:45 +0200

    feat: small changes to design

--- space-monkey-fe/src/components/Rewards/rewardsDashboard/Rewards.js
+++ space-monkey-fe/src/components/Rewards/rewardsDashboard/Rewards.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Title from './Title';
-import PropTypes from 'prop-types';
-import { format } from 'date-fns'
-import Button from '@material-ui/core/Button';
-import Reward from '../images/reward/Reward';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Title from "./Title";
+import PropTypes from "prop-types";
+import { format } from "date-fns";
+import Button from "@material-ui/core/Button";
+import Reward from "../images/reward/Reward";
 
 const useStyles = makeStyles({
   button: {
-    marginTop: '1rem',
+    marginTop: "1rem",
   },
-  centered:{
-    marginLeft: 'auto',
-    marginRight: 'auto',
+  centered: {
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   depositContext: {
     flex: 1,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    marginLeft: "auto",
+    marginRight: "auto",
   },
 });
 
@@ -28,18 +28,16 @@ export default function Rewards(props) {
   const classes = useStyles();
   return (
     <>
-          <div className={classes.centered}>
-            <Title>
-              YOUR REWARDS
-            </Title>
-          </div>
-          <Grid container spacing={3}>
-            <Grid item xs={3} />
-            <Grid item xs={6}>
-              <Reward />
-            </Grid>
-          <Grid item xs={3} />
-    </Grid>
+      <div className={classes.centered}>
+        <Title>YOUR REWARDS</Title>
+      </div>
+      <Grid container spacing={3}>
+        <Grid item xs={3} />
+        <Grid item xs={6}>
+          <Reward />
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
       {/* <Typography component="p" variant="h5" className={classes.centered}>
         {`${reward} BNB`}
       </Typography>
@@ -52,7 +50,9 @@ export default function Rewards(props) {
         color="primary"
         size="large"
         disabled={disabled}
-        onClick={() => { onClaimReward() }}
+        onClick={() => {
+          onClaimReward();
+        }}
       >
         {`${reward} BNB`}
       </Button>
@@ -65,5 +65,5 @@ Rewards.propTypes = {
   nextAvailableClaimDate: PropTypes.instanceOf(Date),
   balance: PropTypes.string,
   onClaimReward: PropTypes.func.isRequired,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
 };
