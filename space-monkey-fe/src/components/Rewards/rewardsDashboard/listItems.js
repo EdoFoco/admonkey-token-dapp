cdaa8bb7145ed3f3ba9fb47d1ac0351308d6128d
Andrea Longhitano <andlon@MAC-UK-ANDLON.station>
2021-07-09 15:01:27 +0200

    Removed rebase leftover <<<<

--- space-monkey-fe/src/components/Rewards/rewardsDashboard/listItems.js
+++ space-monkey-fe/src/components/Rewards/rewardsDashboard/listItems.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -13,19 +12,6 @@ import InfoIcon from '@material-ui/icons/Info';
 import TelegramIcon from '@material-ui/icons/Telegram';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import FacebookIcon from '@material-ui/icons/Facebook';
-=======
-import React from "react";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import HistoryIcon from "@material-ui/icons/History";
-import EqualizerIcon from "@material-ui/icons/Equalizer";
-import InfoIcon from "@material-ui/icons/Info";
->>>>>>> 20db731fee40eeb44209ffaf0a9823a0696230e2
 
 function UnstyledAnchor({ children, url }) {
   return (
