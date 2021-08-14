d390c985d1ea10f17a4e5f4c06da56ffc3a6a89e
Edoardo Foco <e.foco@hotmail.it>
2021-08-14 09:14:12 +0200

    feat: add V2 integration

--- space-monkey-fe/src/components/Rewards/RewardsContainer.js
+++ space-monkey-fe/src/components/Rewards/RewardsContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useWeb3Modal from "../../hooks/useWeb3Modal";
 import Dashboard from "./rewardsDashboard/Dashboard";
 import AdMonkey from "../../contracts/AdMonkey";
+import AdMonkeyV2 from "../../contracts/AdMonkeyV2";
 import { getTokenTransactionsForWallet } from "../../services/bsscan";
 
 export default function RewardsContainer() {
@@ -13,6 +14,8 @@ export default function RewardsContainer() {
   const [transactions, setTransactions] = useState([]);
   const [invalidChain, setInvalidChain] = useState();
   const [adMonkey, setAdMonkey] = useState();
+  const [adMonkeyV2, setAdMonkeyV2] = useState();
+  const [v2Balance, setV2Balance] = useState();
   const [loading, setLoading] = useState(true);
   const [chainId, setChainId] = useState(null);
   const [claimedRewardTransaction, setClaimedRewardTransaction] =
@@ -31,6 +34,8 @@ export default function RewardsContainer() {
       setInvalidChain(false);
       setProvider(provider);
       loadAdMonkey(provider, selectedAccount);
+      loadAdMonkeyV2(provider, selectedAccount);
+      setLoading(false);
     },
   });
 
@@ -63,7 +68,19 @@ export default function RewardsContainer() {
       setBalance(Math.round((balance / 10 ** 9) * 1000, 6) / 1000);
       setNextAvailableClaimDate(nextAvailableClaimDate);
       setTransactions(transactions);
-      setLoading(false);
+    }
+  };
+
+  const loadAdMonkeyV2 = async (provider, account) => {
+    setChainId(JSON.stringify(account));
+    if (provider) {
+      const adMonkeyContract = new AdMonkeyV2(provider.provider, account);
+      await setAdMonkeyV2(adMonkeyContract);
+
+      const balance = await adMonkeyContract.getBalance();
+
+      setV2Balance(Math.round((balance / 10 ** 9) * 1000, 6) / 1000);
+      console.log(`V2 balance is: ${balance}`);
     }
   };
 
